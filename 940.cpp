53f2f572e2c6701bb7cf781b079fb719fa86a274
Sushovan <masantasushovan@gmail.com>
2026-09-07 21:33:22 +0530

    940. Distinct Subsequences II
    Implement distinct subsequences calculation in Solution class.

---
+++ 940.cpp
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+  int distinctSubseqII(string s) {
+    constexpr int kMod = 1'000'000'007;
+    vector<long> endsIn(26);
+
+    for (const char c : s)
+      endsIn[c - 'a'] = accumulate(endsIn.begin(), endsIn.end(), 1L) % kMod;
+
+    return accumulate(endsIn.begin(), endsIn.end(), 0L) % kMod;
+  }
+};
+
+
